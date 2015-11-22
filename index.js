f5e7214d86c427e4a99a386e556e7c8c404754f5
Matthew Liberty <msliberty@mail.umhb.edu>
2015-11-22 11:34:00 -0600

    Fix indentation :P

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 
 window.onload = function() {
-    var canvas = document.getElementById('canvas');
-    var gl = WebGLUtils.setupWebGL(canvas);
+	var canvas = document.getElementById('canvas');
+	var gl = WebGLUtils.setupWebGL(canvas);
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0, 0, 0, 1);
-    gl.enable(gl.DEPTH_TEST);
+	gl.viewport(0, 0, canvas.width, canvas.height);
+	gl.clearColor(0, 0, 0, 1);
+	gl.enable(gl.DEPTH_TEST);
 
 	var program = initShaders(gl, 'vertex-shader', 'fragment-shader');
 	var positionAttribute = gl.getAttribLocation(program, 'position');
