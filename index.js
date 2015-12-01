4a0e77936f4cab01a0bd82a83798d53715a9d186
Matthew Liberty <msliberty@mail.umhb.edu>
2015-12-01 11:53:03 -0600

    +5 to Block picking (fix bug)

--- index.js
+++ index.js
@@ -107,6 +107,31 @@ window.onload = function() {
 		return [x, y, z];
 	}
 
+	function viewMatrix() {
+		return lookAt(cameraPosition(), cameraTarget, [0, 0, 1]);
+	}
+
+	function projectionMatrix() {
+		return perspective(60, canvas.width / canvas.height, 0.01, 100);
+	}
+
+
+	//////////////////// Model ////////////////////
+
+	var placedBricks = [];
+	var nextBrickId = 0;
+
+	function placeBrick(brick) {
+		var id = nextBrickId++;
+		brick.id = id;
+		placedBricks[id] = brick;
+	}
+
+
+	placeBrick({x: 0, y: 0, width: 3, depth: 2, color: [1, 0, 0, 1]});
+	placeBrick({x:5, y:27, width: 2, depth: 4, color: [0, 1, 0, 1]});
+	placeBrick({x:15, y:15, width: 2, depth: 2, color: [.4, .2, .6, 1]});
+
 
 	//////////////////// Input ////////////////////
 
@@ -183,13 +208,15 @@ window.onload = function() {
 
 	var mainProgram = initShaders(gl, 'vertex-shader', 'fragment-shader');
 	var positionAttribute = gl.getAttribLocation(mainProgram, 'position');
-	var modelViewUniform = gl.getUniformLocation(mainProgram, 'modelView');
+	var modelUniform = gl.getUniformLocation(mainProgram, 'model');
+	var viewUniform = gl.getUniformLocation(mainProgram, 'view');
 	var projectionUniform = gl.getUniformLocation(mainProgram, 'projection');
 	var colorUniform = gl.getUniformLocation(mainProgram, 'color');
 
 	var pickBufferProgram = initShaders(gl, 'pick-buffer-vertex-shader', 'pick-buffer-fragment-shader');
 	var pickBufferPositionAttribute = gl.getAttribLocation(pickBufferProgram, 'position');
-	var pickBufferModelViewUniform = gl.getUniformLocation(pickBufferProgram, 'modelView');
+	var pickBufferModelUniform = gl.getUniformLocation(pickBufferProgram, 'model');
+	var pickBufferViewUniform = gl.getUniformLocation(pickBufferProgram, 'view');
 	var pickBufferProjectionUniform = gl.getUniformLocation(pickBufferProgram, 'projection');
 	var pickBufferObjectIdUniform = gl.getUniformLocation(pickBufferProgram, 'objectId');
 
@@ -203,28 +230,36 @@ window.onload = function() {
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 
+	var pickBufferRenderbuffer = gl.createRenderbuffer();
+	gl.bindRenderbuffer(gl.RENDERBUFFER, pickBufferRenderbuffer);
+	gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, canvas.width, canvas.height);
+
 	var pickBuffer = gl.createFramebuffer();
 	gl.bindFramebuffer(gl.FRAMEBUFFER, pickBuffer);
 	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, pickBufferTexture, 0);
+	gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, pickBufferRenderbuffer);
 	gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 
 	function readFromPickBuffer(x, y) {
 		gl.bindFramebuffer(gl.FRAMEBUFFER, pickBuffer);
 		gl.useProgram(pickBufferProgram);
 		gl.clearColor(0, 0, 0, 0);
-		gl.clear(gl.COLOR_BUFFER_BIT);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-		var modelViewMatrix = lookAt(cameraPosition(), cameraTarget, [0, 0, 1]);
-		gl.uniformMatrix4fv(pickBufferModelViewUniform, false, flatten(modelViewMatrix));
+		for (var id in placedBricks) {
+			var brick = placedBricks[id];
 
-		var projectionMatrix = perspective(60, canvas.width / canvas.height, 0.01, 100);
-		gl.uniformMatrix4fv(pickBufferProjectionUniform, false, flatten(projectionMatrix));
+			var modelMatrix = translate(brick.x, brick.y, 0);
+			gl.uniformMatrix4fv(pickBufferModelUniform, false, flatten(modelMatrix));
+			gl.uniformMatrix4fv(pickBufferViewUniform, false, flatten(viewMatrix()));
+			gl.uniformMatrix4fv(pickBufferProjectionUniform, false, flatten(projectionMatrix()));
+			gl.uniform1i(pickBufferObjectIdUniform, id);
 
-		gl.bindBuffer(gl.ARRAY_BUFFER, exampleBrickPositionBuffer);
-		gl.enableVertexAttribArray(pickBufferPositionAttribute);
-		gl.vertexAttribPointer(pickBufferPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-		gl.uniform1i(pickBufferObjectIdUniform, 1);//TODO: id for clickable object should go here
-		gl.drawArrays(gl.TRIANGLES, 0, exampleBrickPositions.length);
+			gl.bindBuffer(gl.ARRAY_BUFFER, exampleBrickPositionBuffer);
+			gl.enableVertexAttribArray(pickBufferPositionAttribute);
+			gl.vertexAttribPointer(pickBufferPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+			gl.drawArrays(gl.TRIANGLES, 0, exampleBrickPositions.length);
+		}
 
 		var pixels = new Uint8Array(4);
 		y = canvas.height - y; // invert y because textures have (0,0) at bottom left, but html has it at top left
@@ -245,28 +280,41 @@ window.onload = function() {
 	gl.enable(gl.DEPTH_TEST);
 	gl.useProgram(mainProgram);
 
+
 	function redraw() {
 		gl.clearColor(0, 0, 0, 1);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-		var modelViewMatrix = lookAt(cameraPosition(), cameraTarget, [0, 0, 1]);
-		gl.uniformMatrix4fv(modelViewUniform, false, flatten(modelViewMatrix));
+		gl.uniformMatrix4fv(viewUniform, false, flatten(viewMatrix()));
+		gl.uniformMatrix4fv(projectionUniform, false, flatten(projectionMatrix()));
 
-		var projectionMatrix = perspective(60, canvas.width / canvas.height, 0.01, 100);
-		gl.uniformMatrix4fv(projectionUniform, false, flatten(projectionMatrix));
+		drawBoard();
+		for (var id in placedBricks)
+			drawBrick(placedBricks[id]);
+		//drawPeg();
+	}
 
+	function drawBoard() {
 		gl.bindBuffer(gl.ARRAY_BUFFER, boardPositionBuffer);
 		gl.enableVertexAttribArray(positionAttribute);
 		gl.vertexAttribPointer(positionAttribute, 3, gl.FLOAT, false, 0, 0);
+		gl.uniformMatrix4fv(modelUniform, false, flatten(mat4()));
 		gl.uniform4fv(colorUniform, flatten(boardColor));
 		gl.drawArrays(gl.TRIANGLES, 0, boardPositions.length);
+	}
+
+	function drawBrick(brick) {
+		var modelMatrix = translate(brick.x, brick.y, 0);
+		gl.uniformMatrix4fv(modelUniform, false, flatten(modelMatrix));
+		gl.uniform4fv(colorUniform, flatten(brick.color));
 
 		gl.bindBuffer(gl.ARRAY_BUFFER, exampleBrickPositionBuffer);
 		gl.enableVertexAttribArray(positionAttribute);
 		gl.vertexAttribPointer(positionAttribute, 3, gl.FLOAT, false, 0, 0);
-		gl.uniform4fv(colorUniform, flatten(exampleBrickColor));
 		gl.drawArrays(gl.TRIANGLES, 0, exampleBrickPositions.length);
+	}
 
+	function drawPeg() {
 		gl.bindBuffer(gl.ARRAY_BUFFER, pegPositionBuffer);
 		gl.enableVertexAttribArray(positionAttribute);
 		gl.vertexAttribPointer(positionAttribute, 3, gl.FLOAT, false, 0, 0);
