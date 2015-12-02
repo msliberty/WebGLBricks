6d8f59d68165e6bdde4d6f42c9632fabf0ed2424
caitlyn31 <caitlyn31@gmail.com>
2015-12-02 17:43:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
 
-//added in  color index 
 var index = 0;
 // color options 
 	var colors = [
@@ -12,7 +11,7 @@ var index = 0;
 		vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
 		vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
 	];
-
+	
 window.onload = function() {
 	var canvas = document.getElementById('canvas');
 	var gl = WebGLUtils.setupWebGL(canvas);
@@ -133,22 +132,25 @@ window.onload = function() {
 
 	var placedBricks = [];
 	var nextBrickId = 0;
-
+		
 	function placeBrick(brick) {
+	
 		var id = nextBrickId++;
 		brick.id = id;
 		placedBricks[id] = brick;
+		
+			
 	}
-
-
+	
 	placeBrick({x: 0, y: 0, width: 3, depth: 2, color: [1, 0, 0, 1]});
 	placeBrick({x:5, y:27, width: 2, depth: 4, color: [0, 1, 0, 1]});
 	placeBrick({x:15, y:15, width: 2, depth: 2, color: [.4, .2, .6, 1]});
-		
+	
 
-	// ADDED IN CODE TO ADD BRICKS
+	// ADDED IN NEW CODE TO ADD BRICKS
 	//added in the new variables
-	//need to store the new blocks created here as well
+	//to create repeated new blocks add in a loop or something
+	// also need to save the new blocks somewhere
 	
 	function newBrick(){
 		var bwidth = document.getElementById("newBlockWidth").value;
@@ -159,6 +161,7 @@ window.onload = function() {
 		placeBrick({x: bx, y: by, width: bwidth, depth: bdepth, color: colors[index%7]});
 	};
 
+	
 	//////////////////// Input ////////////////////
 
 	canvas.oncontextmenu = function(e) { e.preventDefault(); } // capture right-click
@@ -266,6 +269,8 @@ window.onload = function() {
 	gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, pickBufferRenderbuffer);
 	gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 
+	
+	
 	function readFromPickBuffer(x, y) {
 		gl.bindFramebuffer(gl.FRAMEBUFFER, pickBuffer);
 		gl.useProgram(pickBufferProgram);
@@ -330,6 +335,7 @@ window.onload = function() {
 	}
 
 	function drawBrick(brick) {
+		
 		var modelMatrix = translate(brick.x, brick.y, 0);
 		gl.uniformMatrix4fv(modelUniform, false, flatten(modelMatrix));
 		gl.uniform4fv(colorUniform, flatten(brick.color));
@@ -339,14 +345,15 @@ window.onload = function() {
 		gl.vertexAttribPointer(positionAttribute, 3, gl.FLOAT, false, 0, 0);
 		gl.drawArrays(gl.TRIANGLES, 0, exampleBrickPositions.length);
 	}
+	
 
-	function drawPeg() {
-		gl.bindBuffer(gl.ARRAY_BUFFER, pegPositionBuffer);
-		gl.enableVertexAttribArray(positionAttribute);
-		gl.vertexAttribPointer(positionAttribute, 3, gl.FLOAT, false, 0, 0);
-		gl.uniform4fv(colorUniform, flatten(exampleBrickColor));
-		gl.drawArrays(gl.TRIANGLES, 0, pegPositions.length);
-	}
+	//function drawPeg() {
+//		gl.bindBuffer(gl.ARRAY_BUFFER, pegPositionBuffer);
+//		gl.enableVertexAttribArray(positionAttribute);
+//		gl.vertexAttribPointer(positionAttribute, 3, gl.FLOAT, false, 0, 0);
+//		gl.uniform4fv(colorUniform, flatten(exampleBrickColor));
+//		gl.drawArrays(gl.TRIANGLES, 0, pegPositions.length);
+//	}
 
 	function handleAnimationFrame() {
 		requestAnimationFrame(handleAnimationFrame);
